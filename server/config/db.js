b33d7f9936e3f3328d1b51ee5a37538ce3368d46
skpremkumar2005pnr <premkumar.cb22@bitsathy.ac.in>
2023-09-07 19:04:12 +0530

    add

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 try {
-  mongoose.connect("mongodb://localhost:27017/Ecommerce", {
+  mongoose.connect("mongodb+srv://premkumarcb22:s.p2r0e0m5@cluster0.dvqkp70.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
